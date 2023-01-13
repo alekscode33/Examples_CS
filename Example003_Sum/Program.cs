92807fb4051dc952c8c1e5e3fff08ddeaa698cd7
Alexander Sit <120254020+alekscode33@users.noreply.github.com>
2023-01-13 17:25:17 +1100

    Example 003 Sum of two numbers

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,7 @@
+﻿int numberA = 31;
+int numberB = 5;
+Console.Write("numberA + numberB =");
+Console.WriteLine(numberA + numberB);
+int result = numberA + numberB;
+Console.Write("result = ");
+Console.WriteLine(result);
