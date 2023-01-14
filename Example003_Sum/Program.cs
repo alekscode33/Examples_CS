58e8ebfb2f608065ace62955f04c144edc6cbcd3
Alexander Sit <120254020+alekscode33@users.noreply.github.com>
2023-01-15 03:39:24 +1100

    used random generator within Example003_Sum for addition two random numbers

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,7 +1,26 @@
-﻿int numberA = 31;
-int numberB = 5;
-Console.Write("numberA + numberB =");
-Console.WriteLine(numberA + numberB);
+﻿//int numberA = 31;
+//int numberB = 5;
+//Console.Write("numberA + numberB =");
+//Console.WriteLine(numberA + numberB);
+//int result = numberA + numberB;
+//Console.Write("result = ");
+//Console.WriteLine(result);
+
+
+
+//int numberA = new Random().Next(1, 10);
+// 1 2 3 … 9 случайное целое число из диапазона от 1 до 10
+//int numberB = new Random().Next(1, 10);
+// 1 2 3 … 9 случайное целое число из диапазона от 1 до 10
+//int result = numberA + numberB;
+//Console.WriteLine(result);
+
+
+// Используем генератор случайных чисел
+// int NumberX = new Random().Next(min,max-1); 
+int numberA = new Random().Next(1, 10); // от 1 2 3 до 9 случайное целое число из диапазона от 1 до 10
+Console.WriteLine(numberA); // WriteLine — вывод с переходом на новую строку
+int numberB = new Random().Next(1, 10); // 1 2 3 … 9 случайное целое число из диапазона от 1 до 10
+Console.WriteLine(numberB);
 int result = numberA + numberB;
-Console.Write("result = ");
-Console.WriteLine(result);
+Console.Write(result);// Write — это вывод без перехода на новую строку. 
