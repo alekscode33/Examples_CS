5d594884a42e1ed4ba5fee11c0c45e3ca49d7727
Alexander Sit <120254020+alekscode33@users.noreply.github.com>
2023-01-15 04:59:08 +1100

    Example 005 Condition If-Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Please, input your name: ");
+string username = Console.ReadLine();
+
+//if (username == "Masha")  
+// condition doesn't work properly cause many ways to type
+// favorite username (e.g. MASHA masha mASHA ... and so on)
+
+if (username.ToLower() == "masha")
+// using the .ToLower() allows our program to convert all
+// letters of our username to lower case before checking the condition
+// now any variants of name is recognized 
+{
+    Console.WriteLine("Hura!!! This is our MASHA!!!");
+}
+else
+{
+    Console.WriteLine("Hi, ");
+    Console.WriteLine(username);
+}
