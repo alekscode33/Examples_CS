e739d1624d6d48157ceada71fff228d1beee8937
Alexander Sit <120254020+alekscode33@users.noreply.github.com>
2023-01-23 23:53:46 +1100

    Added Example 009 Intro in Arrays

--- Example008_1_IntroMethod_using/Program.cs
+++ Example008_1_IntroMethod_using/Program.cs
@@ -13,18 +13,20 @@ int a1 = 15;
 int b1 = 21;
 int c1 = 39;
 int a2 = 12;
-int b2 = 2311;
+int b2 = 23;
 int c2 = 33;
 int a3 = 13;
-int b3 = 23111;
-int c3 = 313;
+int b3 = 23;
+int c3 = 33;
 
 // int max1 = Max(a1, b1, c1);
 // int max2 = Max(a2, b2, c2);
 // int max3 = Max(a3, b3, c3);
 // int max = Max(max1, max2, max3);
 
-int max = Max(a1, b1)
-
+int max = Max(
+    Max(a1, b1, c1),
+    Max(a2, b2, c2),
+    Max(a3, b3, c3));
 
 Console.WriteLine(max);
---
+++ Example009_IntroArrays/Program.cs
@@ -0,0 +1,29 @@
+﻿// How to find Maximum from 9 numbers 
+// using methods
+
+int Max(int arg1, int arg2, int arg3)   // Method which used to compare numbers 
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+// int [] array = {1,2,3,4,5,6,7,8,9}; 
+// Сейчас это 9 значений, идущих по порядку. 
+// Далее правим значения и убираем лишнее:
+
+//               0   1   2   3   4   5   6   7   8
+int[] array = { 11, 21, 31, 41, 15, 61, 17, 18, 19 };
+
+int max = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8]));
+
+Console.WriteLine(max);
+
+
+
+
+
