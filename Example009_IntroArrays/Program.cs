27f2e28177fdd102218ae4d5ee817f87935ec583
Alexander Sit <120254020+alekscode33@users.noreply.github.com>
2023-02-06 22:51:44 +1100

    Example 012 Methods

--- Example009_IntroArrays/Program.cs
+++ Example009_IntroArrays/Program.cs
@@ -14,14 +14,20 @@ int Max(int arg1, int arg2, int arg3)   // Method which used to compare numbers
 // Далее правим значения и убираем лишнее:
 
 //               0   1   2   3   4   5   6   7   8
-int[] array = { 11, 21, 31, 41, 15, 61, 17, 18, 19 };
+int[] array = { 11, 21, 31, 4134, 15, 6134, 17, 18, 19 };
 
-int max = Max(
+//array[0] = 12;                 // how to replace the value of array's 
+// element with index [0] from 11 to 12
+
+//Console.WriteLine(array[0]);   // this line shows via Console 
+// the value of array's element with index [0]  -> now it's equals 12
+
+int result = Max(
     Max(array[0], array[1], array[2]),
     Max(array[3], array[4], array[5]),
     Max(array[6], array[7], array[8]));
 
-Console.WriteLine(max);
+Console.WriteLine(result);
 
 
 
